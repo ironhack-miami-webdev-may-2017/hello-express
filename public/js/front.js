486b77355427f3c7c1dd26fc6131483160054eb6
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-06-21 16:37:03 -0400

    Add basic Express Website

---
+++ public/js/front.js
@@ -0,0 +1,10 @@
+// THIS IS FRONT END JS CODE
+
+//                                   $('img')
+const myImage = document.querySelector('img');
+
+// ALERT when an image is clicked
+//               $('img').click(  () => {
+myImage.addEventListener('click', () => {
+  alert('GIF not JIF');
+});
